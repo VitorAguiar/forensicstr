354f7adca5ed196c8376eb4e13871a458d88997a
Vitor <vitor.aguiar@me.com>
2014-04-24 00:10:03 -0300

    Update Rforensic.R

--- R/Rforensic.R
+++ R/Rforensic.R
@@ -197,7 +197,7 @@ for(i in seq(2,ncol(pop),2)) {
 	missing <- c(missing, m)
 }
 
-names(missing) <- names(pop)[seq(2,ncol(pop),2)]
+names(missing) <- lnames
 # Saving the plot as .png with 1200 dpi of resolution to the working directory
 png("figure1.png", width=3.25, height=3.25, units="in", res=1200, pointsize=6, family="Arial")
 par(mar = c(7,4,2,1))
